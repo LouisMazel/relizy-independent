e8e92678f0ae1167ea9cbcd02f9de358e1f7e8ee
LouisMazel <me@loicmazuel.com>
2025-11-14 18:51:51 +0100

    feat: init repo for testing relizy in independent mode

---
+++ relizy.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'relizy'
+
+export default defineConfig({
+  cwd: './',
+  types: {
+    feat: { title: '🚀 Features', semver: 'minor' },
+    perf: { title: '🔥 Performance', semver: 'patch' },
+    fix: { title: '🩹 Fixes', semver: 'patch' },
+    refactor: { title: '💅 Refactors', semver: 'patch' },
+    types: { title: '🌊 Types', semver: 'patch' },
+    style: { title: '💄 Styles', semver: 'patch' },
+    docs: { title: '📖 Documentation' },
+    test: { title: '🧪 Tests' },
+    chore: false,
+    examples: false,
+    ci: false,
+    build: false,
+  },
+
+  monorepo: {
+    versionMode: 'independent',
+    packages: ['./packages/*'],
+  },
+
+  release: {
+    publish: false,
+  },
+})
